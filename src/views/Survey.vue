edf527a90f6ca942b67b77d496073415fb5d8f7c
Mumeino <mumeino.iikigen@gmail.com>
2019-08-29 00:56:15 +0700

    ตัวอย่างการดึงไฟล์ excel ของ survey

--- src/views/Survey.vue
+++ src/views/Survey.vue
@@ -5,8 +5,27 @@
 </template>
 
 <script>
+// import axios from '@/axios.js'
+
 export default {
-  name: 'survey'
+  name: 'survey',
+  // created () {
+  //   // test get file excel
+  //   axios({
+  //     method: 'post',
+  //     url: '/admin/survey/full',
+  //     data: {
+  //       // id: [1,2]
+  //       id: 'all'
+  //     }
+  //   }).then(response => {
+  //     console.log('survey', response.data)
+  //     const url = response.data.url
+  //     // window.open(url, 'blank')
+  //   }).catch(error => {
+  //     console.error('survey', error.message, error.response.data)
+  //   })
+  // }
 }
 </script>
 
