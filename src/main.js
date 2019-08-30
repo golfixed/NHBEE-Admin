f1fae84d208ec2661c7b84599ab6d68c87a53465
Mumeino <mumeino.iikigen@gmail.com>
2019-08-30 20:40:46 +0700

    เพิ่มตัวอย่าง Vue Analytics

--- src/main.js
+++ src/main.js
@@ -12,6 +12,16 @@ if (token) {
   axios.defaults.headers.common['Authorization'] = 'Bearer ' + token
 }
 
+// ตัวอย่างการใช้ Vue Analytics
+// import VueAnalytics from 'vue-analytics'
+// Vue.use(VueAnalytics, {
+//   id: 'UA-146666158-1', // id นี้คือ NHBEE KMUTT แล้ว
+//   router,
+//   debug: {
+//     sendHitTask: process.env.NODE_ENV === 'production'
+//   }
+// })
+
 new Vue({
   router,
   store,
