2577c4e5e6970938fd1a89190d95c728e86025e9
Mumeino <mumeino.iikigen@gmail.com>
2019-08-30 20:28:32 +0700

    ดึงข้อมูล Analytic

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,38 @@
 <template>
   <div v-if="isLoggedIn" class="home-display isdesktop ismobile">
     Dashbaord
+    <p v-if="analyticData">{{ analyticData }}</p>
   </div>
 </template>
 
 <script>
 import layout_default from '@/layouts/main.vue'
 import layout_login from '@/layouts/login.vue'
+import axios from '@/axios.js'
 
 export default {
   name: "home",
   components: {},
+  data () {
+    return {
+      analyticData: {
+        all: {},
+        dimensions: {}
+      }
+    }
+  },
   created () {
-    if (this.isLoggedIn) this.$emit(`update:layout`, layout_default)
-    else this.$emit(`update:layout`, layout_login)
+    if (this.isLoggedIn) {
+      this.$emit(`update:layout`, layout_default)
+      this.getAnalyticData()
+    } else this.$emit(`update:layout`, layout_login)
   },
   watch: {
     isLoggedIn (newval) {
-      if (newval) this.$emit(`update:layout`, layout_default)
-      else this.$emit(`update:layout`, layout_login)
+      if (newval) {
+        this.$emit(`update:layout`, layout_default)
+        this.getAnalyticData()
+      } else this.$emit(`update:layout`, layout_login)
     }
   },
   computed: {
@@ -27,8 +41,14 @@ export default {
     }
   },
   methods: {
-    refreshToken () {
-      this.$store.getters.forceRefresh()
+    getAnalyticData () {
+      axios('/admin/dashboard/analytic').then(response => {
+        this.analyticData = response.data
+      }).catch(error => {
+        if (error.response && error.response.data)
+          console.error("get analytic", error.response.data.error);
+        else console.error("get analytic", error.message);
+      })
     }
   }
 };
